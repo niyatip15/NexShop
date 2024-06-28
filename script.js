2ca3df9a2081e54bf3906068c13ed76be4b108b0
Niyati Pathak <niyupathak1503@gmail.com>
2024-06-28 12:11:38 +0530

    responsive sidebar menu.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Script for navigation bar
+const menu = document.getElementById('burger-menu');
+const closeMenu = document.getElementById('close')
+const nav =  document.getElementById('navbar');
+
+if(menu){
+    menu.addEventListener('click',() =>{
+        nav.classList.add('active')
+    })
+}
+if(closeMenu){
+    closeMenu.addEventListener('click',() =>{
+        nav.classList.remove('active')
+        })
+}
